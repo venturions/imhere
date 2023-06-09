1ddf54872692b7c5904610d552f7ba25ae09d46f
venturions <alex.eduardo10@hotmail.com>
2023-06-09 16:00:49 -0300

    estilização com stylesheet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Nome do Evento</Text>
+      <Text style={styles.subtitle}>Sexta, 4 de Novembro de 2022.</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24,
   },
-});
+  title: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  subtitle: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})
